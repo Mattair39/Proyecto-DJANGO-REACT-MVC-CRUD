06c7bcc7b577ca434f58da0ae84e00c195693246
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2025-04-08 01:21:01 -0500

    Fetch para conectar Frontend y Backend (Books)

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,7 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [books,setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  },[]);
+
+  const fetchBooks = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/books/")
+        const data = await response.json()
+        setBooks(data);
+      } catch (err) {
+        console.log(err);
+      }
+  };
+  
+
   return (
     <>
       <h1> Página de Libros </h1>
@@ -10,6 +27,12 @@ function App() {
         <input type="number" placeholder="Fecha de Publicación" />
         <button> Agregar Libro </button>
       </div>
+    {books.map((book) => (
+      <div>
+        <p>Título: {book.bookTitle}</p>
+        <p>Año de Publicación: {book.release_year} </p>
+      </div>
+    ))}
     </>
   )
 }

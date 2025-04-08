6df952fedc6fe8026aefc667e2b033d7fc9e5fbc
Mattair39 <105869714+Mattair39@users.noreply.github.com>
2025-04-08 17:22:45 -0500

    Desarrollo Método Update (PUT) y Delete (DELETE) y Lógica en Frontend

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
   const [books,setBooks] = useState([]);
+  const [bookTitle, setBookTitle] = useState("");	
+  const [releaseYear, setReleaseYear] = useState(0); 
 
   useEffect(() => {
     fetchBooks();
@@ -18,14 +20,40 @@ function App() {
       }
   };
   
+  const addBook = async () => {
+    const bookData = {
+      bookTitle: bookTitle,
+      release_year: releaseYear
+    };
+    try {
+    const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookData),
+    });
+
+    const data = await response.json();
+    setBooks((prev) => [...prev, data]);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
   return (
     <>
       <h1> Página de Libros </h1>
       <div>
-        <input type="text" placeholder="Título del Libro" />
-        <input type="number" placeholder="Fecha de Publicación" />
-        <button> Agregar Libro </button>
+        <input type="text" 
+        placeholder="Título del Libro" 
+        onChange={(e) => setBookTitle(e.target.value)} 
+        />
+        <input type="number" 
+        placeholder="Año de Publicación" 
+        onChange={(e) => setReleaseYear(e.target.value)}
+        />
+        <button onClick={addBook}> Agregar Libro </button>
       </div>
     {books.map((book) => (
       <div>
